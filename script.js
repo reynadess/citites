8556e92116eae851a8c75ace5f6123ccfd2c3335
Sai Chaitanya Raparti <47269265+reynadess@users.noreply.github.com>
2019-04-02 23:29:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+var city = document.getElementById("city");
+var stat = document.getElementById("state");
+
+var http = new XMLHttpRequest();
+
+http.open("GET", "cities.json", false);
+
+http.send();
+
+var data = JSON.parse(http.responseText);
+
+function states(){
+    var state = document.getElementById("state").value;
+    var x="";
+    for(var i=0; i<data.length; i++){
+        if(state == data[i].state){
+            x = x + " ,"+ data[i].name;
+        }
+        city.innerHTML=x;
+    }
+   
+}
